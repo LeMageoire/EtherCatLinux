abf7de0041acdd022dd6c7157fdbdba634b0e41f
Mathis Guyot <mathis.guyot@hotmail.com>
2024-02-28 10:02:44 +0100

    Multithreadding test of feb 27.02.24 which seems to not alter working behavior

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <pthread.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include "ethercat.h"
@@ -7,6 +9,50 @@
 
 #define NUMBER_OF_SLAVES (2)
 
+#if 0
+void readPDOmapping(uint16 slave)
+{
+    uint8  mapSize;
+    uint32 bytesRead;
+    uint16 index;
+    uint8 subIndex;
+    uint32 value;
+
+    index = 0x1C12;
+    subIndex = 0x00;
+    ec_SDOread(slave, index, subIndex, FALSE, &bytesRead, &mapSize, EC_TIMEOUTRXM);
+    for(int i = 1; i <= mapSize; i++) {
+        ec_SDOread(slave, index, i, FALSE, &bytesRead, &value, EC_TIMEOUTRXM);
+        printf("Mapped RxPDO index at position %d : 0x%04X\n", i , value);
+    }
+}
+#endif 
+
+void set_output_on_EL2798(int slave_position, int output_number){
+    int byte_index = 0;
+    int bit_position = output_number;
+    if(output_number < 0 || output_number >= 8)
+    {    
+        printf("Invalid outputnumber\n");
+        return ;
+    }
+    ec_slave[slave_position].outputs[byte_index] |= (1 << bit_position);
+   // ec_send_processdata();
+   // ec_receive_processdata(EC_TIMEOUTRET);
+}
+void set_output_off_EL2798(int slave_position, int output_number){
+    int byte_index = 0;
+    int bit_position = output_number;
+    if(output_number < 0 || output_number >= 8)
+    {    
+        printf("Invalid outputnumber\n");
+        return ;
+    }
+    ec_slave[slave_position].outputs[byte_index] &= ~(1 << bit_position);
+   // ec_send_processdata();
+   // ec_receive_processdata(EC_TIMEOUTRET);
+}
+#if 0
 int EK1100setup(uint16 slave)
 {
     int retval;
@@ -44,26 +90,127 @@ void set_output_int16(uint16 slave_no, uint8 module_index, int16 value)
     *data_ptr++ = (value >> 0) & 0xFF;
     *data_ptr++ = (value >> 8) & 0xFF;
 }
+#endif
 
-int main()
-{
+typedef struct {
+    int inputData;
+} EtherCAT_Data;
+
+volatile EtherCAT_Data globalData;
+volatile int shouldStop = 0;
+pthread_mutex_t dataMutex;
+
+void *EtherCATThread(void *arg) {
     //const char *ifname = "enp0s31f6";
     const char *ifname = "enp0s31f6";
     if(ec_init(ifname) <= 0)
     {
         printf("ec_init failed\n");   
-        return 1;
+        return NULL;
     }
     if(ec_config_init(FALSE) <= 0)
     {
         printf("ec_config_init failed\n");
-        return 1;
+        return NULL;
     }
     if (ec_slavecount < NUMBER_OF_SLAVES)
     {
         printf("Found %d slaves, expected %d\n", ec_slavecount, NUMBER_OF_SLAVES);
+        return NULL;
+    }
+    printf("Setup is done\n");
+    char IOmap[4096];
+    int usedmem;
+
+    usedmem = ec_config_map(&IOmap);
+    if(usedmem<= sizeof(IOmap))
+        printf("Bonjour %d\n",usedmem);
+    ec_configdc();
+    ec_statecheck(0, EC_STATE_PRE_OP, EC_TIMEOUTSTATE);
+    ec_slave[0].state = EC_STATE_SAFE_OP;
+    ec_writestate(0);
+    int chk = 40;
+    do {
+        ec_send_processdata();
+        ec_receive_processdata(EC_TIMEOUTRET);
+        ec_statecheck(0,EC_STATE_SAFE_OP, 50000);
+    } while(chk-- && ec_slave[0].state != EC_STATE_SAFE_OP);
+
+    if(ec_slave[0].state == EC_STATE_SAFE_OP) {
+        printf("All slaves reached SAFE_OP state.\n");
+    }
+    ec_slave[0].state = EC_STATE_OPERATIONAL;
+    ec_writestate(0);
+    chk = 40;
+    do {
+        ec_send_processdata();
+        ec_receive_processdata(EC_TIMEOUTRET);
+        ec_statecheck(0,EC_STATE_OPERATIONAL, 50000);
+    } while (chk-- && ec_slave[0].state != EC_STATE_OPERATIONAL);
+
+    if(ec_slave[0].state == EC_STATE_OPERATIONAL) {
+        printf("All slaves reached EC_STATE_OPERATIONAL state.\n");
+    }
+
+    printf("Start\n");
+    set_output_off_EL2798(2,4);
+    set_output_off_EL2798(2,3);
+    set_output_off_EL2798(2,5);
+    set_output_off_EL2798(2,6);
+    set_output_off_EL2798(2,7);
+    set_output_on_EL2798(2,0);
+    set_output_on_EL2798(2,1);
+    set_output_on_EL2798(2,2);
+
+    while(!shouldStop) {
+        ec_send_processdata();
+        ec_receive_processdata(EC_TIMEOUTRET);
+        pthread_mutex_lock(&dataMutex);
+        pthread_mutex_unlock(&dataMutex);
+        usleep(1000);
+    }
+    
+    return (NULL);
+}
+
+void *ProcessingThread(void *arg) 
+{
+    while(!shouldStop){
+        pthread_mutex_lock(&dataMutex);
+        printf("%d \n", globalData.inputData);
+        pthread_mutex_unlock(&dataMutex);
+        usleep(5000);
+    }
+    return NULL;
+}
+
+int main() 
+{
+    pthread_t ecThread, processingThread;
+    pthread_mutex_init(&dataMutex, NULL);
+
+    if(pthread_create(&ecThread, NULL, EtherCATThread, NULL)) {
+        fprintf(stderr, "Error creating EtherCat thread \n");
+    }
+
+    if(pthread_create(&processingThread, NULL, ProcessingThread,NULL)) {
+        fprintf(stderr, "Error creating processing thread \n");
         return 1;
     }
+
+    shouldStop = 0;
+    pthread_join(ecThread, NULL);
+    pthread_join(processingThread, NULL);
+    pthread_mutex_destroy(&dataMutex);
+    
+    return 0;
+}
+
+
+#if 0
+int main()
+{
+    
 
     if(strcmp(ec_slave[1].name, "EK1100") != 0)
     {
@@ -77,29 +224,38 @@ int main()
         ec_slave[2].PO2SOconfig = EL2798setup;
         return 1;
     }
-    
+ 
     //SETUP_IS_DONE
-    printf("Setup is done\n");
-    char IOmap[128];
-    int usedmem;
-
-    usedmem = ec_config_map(&IOmap);
-    if(usedmem<= sizeof(IOmap))
-        printf("Bonjour %d\n",usedmem);
-
-
-
-
-    // Get the slave configuration structure for EL2798
-    ec_slavet EL2798 = ec_slave[2];
-
-    // Check if the slave is reachable
-    if (EL2798.state != EC_STATE_OPERATIONAL)
+    
+    while(1)
     {
-        fprintf(stderr, "EL2798 is not in operational state.\n");
-        ec_close();
-        return -1;
+        ec_send_processdata();
+        //printf("receive %d\n",ec_receive_processdata(EC_TIMEOUTRET));
+        osal_usleep(10000);
+        //ec_receive_processdata();
     }
+
+    //while(1);
+    //printf("writestate : %d\n",ec_writestate(0)); 
+    //printf("ec_writestate %d \n",ec_writestate(2));
+    //printf("ec_statecheck %d \n",(ec_statecheck(2, EC_STATE_OPERATIONAL, EC_TIMEOUTSTATE)));
+    //printf("ec_readstate %d \n",ec_readstate());
+
+    //ec_configdc();
+    // Get the slave configuration structure for EL2798
+    //ec_slavet EL2798 = ec_slave[2];
+    
+    // Check if the slave is reachable
+    //printf("ecx_statecheck %d \n",(ecx_statecheck(1, EC_STATE_OPERATIONAL, EC_TIMEOUTSTATE)));
+    //ec_send_processdata();
+    //int volatile wkc = ec_receive_processdata(EC_TIMEOUTRET);
+    //printf("ec_writestate %d \n",ec_writestate(0));
+    //printf("ec_statecheck %d \n",(ec_statecheck(0, EC_STATE_OPERATIONAL, EC_TIMEOUTSTATE)));
+    //ec_writestate(0);
+    //while(ec_statecheck(0,EC_STATE_SAFE_OP, EC_TIMEOUTSTATE) != EC_STATE_SAFE_OP )
+    //    ;
+    //printf("config done\n");
+
 
      // Set the data to turn on the EL2798
     uint8_t enableData = 1;  // You may need to adjust this value based on the EL2798 specifications
@@ -123,17 +279,12 @@ int main()
         return -1;
     }
     
-
-
     // Request the master to send the frame
     ec_send_processdata();
 
     // Wait for the frame to be sent
     ec_receive_processdata(EC_TIMEOUTRET);
-
-    // Close EtherCAT
     ec_close();
-
     return 0;
 }
 
@@ -170,7 +321,6 @@ void simpletest(char *ifname)
  * @param slave 16-bit slave address
  * @return int 1 on success, 0 on failure
  */
-
 int EL7031setup(uint16 slave)
 {
     int retval;
@@ -200,4 +350,5 @@ int EL5152setup(uint16 slave)
     retval = 0;
 }
 
+#endif
 
